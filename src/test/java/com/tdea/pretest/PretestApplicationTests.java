9b0977d3a36e0ff0b01b984ca1b5e133d751b32d
Laura0323 <96506699+Laura0323@users.noreply.github.com>
2022-03-19 11:42:48 -0500

    Update PretestApplicationTests.java

--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -2,10 +2,19 @@ package com.tdea.pretest;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class PretestApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    public void isPasswordEqual() {
 
+        String dbPassword = "hola";
+        String password = "hola";
+
+        boolean result = UserUtils.isPasswordEqual(dbPassword, password);
+        assertTrue(result);
+    }
 }
